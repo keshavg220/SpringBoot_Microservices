55e7ed6ca9c03f40463bdadd630691b08104fa99
keshav220 <59433700+keshavg220@users.noreply.github.com>
2020-09-13 22:44:24 +0530

    chnages

--- src/main/java/com/student/information/system/repository/StudentRepository.java
+++ src/main/java/com/student/information/system/repository/StudentRepository.java
@@ -14,8 +14,6 @@ public interface StudentRepository extends MongoRepository<Student, String> {
 
     Student findByStudentNumber(long studentNumber);
 
-    Student findByEmail(String email);
-
     List<Student> findAllByOrderByGpaDesc();
 
 }
